de275227dea81a8c4b12b1b1d2457daa3b3c404b
zeoio <ifordo@outlook.com>
2025-04-24 17:12:08 +0800

    test 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, rust!");
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn basic_test() {
+        assert_eq!(1 + 1, 2);
+    }
 }
